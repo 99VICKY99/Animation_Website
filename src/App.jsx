dd3b2288995bfc1d22e5ee429436339bab35e716
Vicky <vickyprasad2732003@gmail.com>
2025-09-03 02:37:53 +0530

    feat: update App component to use custom font styles and enhance index.css with font-face definitions

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const App = () => {
   return (
     <main>
-      <h1 className='text-5xl text-orange-500 font-bold'>Welcome to Awards</h1>
+      <h1 className='text-5xl text-orange-500 font-bold font-robert-regular'>Welcome to Awards</h1>
+      <h2  className='text-3xl text-blue-500 mt-4 font-robert-regular'>Testing Direct Font Style</h2>
     </main>
   )
 }
