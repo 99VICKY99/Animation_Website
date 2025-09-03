dae60908ef5f8f75d09fda55874d034d5afc0dd7
Vicky <vickyprasad2732003@gmail.com>
2025-09-03 11:13:49 +0530

    fix: remove redundant font class from App component and enhance index.css with additional color utilities

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <main>
-      <h1 className='text-5xl text-orange-500 font-bold font-robert-regular'>Welcome to Awards</h1>
-      <h2  className='text-3xl text-blue-500 mt-4 font-robert-regular'>Testing Direct Font Style</h2>
+      <h1 className='text-5xl text-violet-300 font-bold '>Welcome to Awards</h1>
     </main>
   )
 }
