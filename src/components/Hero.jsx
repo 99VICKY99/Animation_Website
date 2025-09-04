c14cda043c51dd0b58cbbfa9a828331701a202b8
Vicky <vickyprasad2732003@gmail.com>
2025-09-04 13:20:21 +0530

    feat: enhance Hero component with video loading state and GSAP animations

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,12 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Button from './Button';
 import { TiLocationArrow } from 'react-icons/ti';
 import { useGSAP } from '@gsap/react';
 import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
+
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Hero = () => {
     const [currentIndex, setcurrentIndex] = useState(1);
@@ -20,6 +24,13 @@ const Hero = () => {
 
         setcurrentIndex(upcomingVideoIndex);
     }
+
+   useEffect(() => {
+    if (loadedVideos === totalVideos-1) {
+      setisLoading(false);
+    }
+  }, [loadedVideos]);
+
 
     useGSAP(()=>{
         if(hasClicked){
@@ -46,6 +57,25 @@ const Hero = () => {
         }
     },{dependencies:[currentIndex],revertOnUpdate:true})
 
+    useGSAP(()=>{
+        gsap.set('#video-frame',{
+            clipPath: 'polygon(14% 0%, 72% 0%, 90% 90%, 0% 100%)',
+            borderRadius: '0 0 40% 10%'
+        })
+
+        gsap.from('#video-frame', {
+            clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)',
+            borderRadius: '0 0 0 0 ',
+            ease: 'power1.inOut',
+            scrollTrigger: {
+                trigger: '#video-frame',
+                start: 'center center',
+                end: 'bottom center',
+                scrub: true,
+            }
+        });
+    })
+
     const handleVideoLoad = () => {
         setloadedVideos((prevIndex) => prevIndex + 1);
     }
@@ -54,14 +84,26 @@ const Hero = () => {
         return `videos/hero-${index}.mp4`;
     }
 
+    const miniVideoRef = useRef(null);
+
   return (
-    <div className='relative h-dvh w-screen overflow-x-hidden'>
+    <div className='relative h-dvh w-screen overflow-x-hidden '>
+
+        {isLoading && (
+            <div className='flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-50 '>
+                <div className='three-body'>
+                    <div className='three-body__dot' />
+                    <div className='three-body__dot' />
+                    <div className='three-body__dot' />
+                </div>
+            </div>
+        )}
       <div id="video-frame" className='relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75'>
         <div>
             <div className='mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg'>
                 <div onClick={handleMiniVideoClick} className='origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100'>
                    <video 
-                    ref={nextVideoRef} 
+                    ref={miniVideoRef} 
                     src={getVideoSrc(upcomingVideoIndex)}
                     loop
                     muted
@@ -79,6 +121,7 @@ const Hero = () => {
                 muted
                 id="next-video"
                 className='absolute-center invisible absolute z-20 size-64 object-cover object-center'
+                onLoadedData={handleVideoLoad}
             />
 
             <video
@@ -103,10 +146,10 @@ const Hero = () => {
         </div>
       </div>
       <h1 className='special-font hero-heading absolute bottom-5 right-5  text-black'>
-            G<b>a</b>ming
+            G<b>A</b>ming
         </h1>
     </div>
-  )
-}
+  );
+};
 
 export default Hero
